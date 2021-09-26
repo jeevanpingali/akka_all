c65e656149402f3ade6e2d2365065f09d3478d30
Jeevan Pingali <jeevan.pingali@gmail.com>
2021-09-26 10:46:41 +0530

    Now using Actor logging, using context logging in both actors

--- src/main/scala/pingali/jeevan/learning/akka/events/OrderPlacedActor.scala
+++ src/main/scala/pingali/jeevan/learning/akka/events/OrderPlacedActor.scala
@@ -2,18 +2,16 @@ package pingali.jeevan.learning.akka.events
 
 import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.Behaviors
-import akka.event.slf4j.Logger
 import all_events.AllEvents.OrderPlaced
 
 object OrderPlacedActor {
-  val logger= Logger("Order Placed Actor")
 
   def apply(): Behavior[OrderPlaced] =
     Behaviors.setup { context =>
-      logger.info(s"Context: $context")
+      context.log.info("Context: {}", context)
 
       Behaviors.receiveMessage { message =>
-        logger.info(s"Received message: ${message.getName}")
+        context.log.info("Received message: {}", message.getName)
         Behaviors.same
       }
     }
--- src/main/scala/pingali/jeevan/learning/akka/events/PersonActor.scala
+++ src/main/scala/pingali/jeevan/learning/akka/events/PersonActor.scala
@@ -2,18 +2,16 @@ package pingali.jeevan.learning.akka.events
 
 import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.Behaviors
-import akka.event.slf4j.Logger
 import all_events.AllEvents.Person
 
 object PersonActor {
-  val logger = Logger("Person Actor")
 
   def apply(): Behavior[Person] =
     Behaviors.setup { context =>
-      logger.info(s"Context: $context")
+      context.log.info(s"Context: $context")
 
       Behaviors.receiveMessage { message =>
-        logger.info(s"Received message: ${message.getName}")
+        context.log.info(s"Received message: ${message.getName}")
         Behaviors.same
       }
     }
